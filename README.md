f36886e62a4651e250bc9d267eecb4f3f398fb53
Erik Stone <spologas@gmail.com>
2021-08-19 13:40:31 -0700

    added to the elements that will draw and fill out todays weather card and linked names and ids to help the info go where it needs to. Also added css to help everything stay on screen

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Weather-Dashboard-App
 This is a Weather Dashboard App that only uses html, css, JavaScript, jQuery and a Weather API called Open Weather
+This homework has the following functions:
+The app is able to search for a city and display the current weather along with a five day forcast.
+The displayed weather cards will have: the city name, the date, an icon to represent weather conditions, the temperature, the humidity, wind speed, and the UV index.
+the UV index will be expandable and then using a color system, display if it is favorable, moderate, or severe.
+The five day forcast cards will display: the date, an icon representing weather conditions, the temperature, the humidity, wind speed.
+This app will also allow the user to use a search history, that will show the current and future weather conditions.
--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-function initpage() {
-  const cityInputBar = document.querySelector('cityInputBar');
-  const searchBtn = document.querySelector('searchBtn');
-  const clearHistBtn = document.querySelector('clearHistoryButton');
-  const cityName = document.querySelector('city-name');
-  const todaysImg = document.querySelector('weather-img');
-  const todaysTemp = document.querySelector('temperature');
-  const todaysHumidity = document.querySelector('humidity');
-  const todaysWind = document.querySelector('wind');
-  const todaysUV = document.querySelector('UV-index');
+function initPage() {
+  const cityInputBar = document.querySelector('#cityInputBar');
+  const searchBtn = document.querySelector('#searchBtn');
+  const clearHistBtn = document.querySelector('#clearHistBtn');
+  const cityName = document.querySelector('#cityName');
+  const date = document.querySelector('#date');
+  const today = document.querySelector('#currentDay');
+  const todaysImg = document.querySelector('#weatherImg');
+  const todaysTemp = document.querySelector('#temp');
+  const todaysHumidity = document.querySelector('#hum');
+  const todaysWind = document.querySelector('#wind');
+  const todaysUV = document.querySelector('#UV');
   const searchHistory = document.querySelector('search-history');
-  let historySearch = JSON.parse(localStorage.getItem('history'))
+  let historySearch = JSON.parse(localStorage.getItem('history')) || [];
+  const timeZoneOffSet=0
   console.log(historySearch);
-
   const APIKey = 'd888e695283db928ef9b9fd7d40936fc';
-  
 }
+
+function generateMeteorologist (e) {
+  e.preventDefault()
+  console.log(cityInputBar.value)
+    const requestUrl = `https://api.openweathermap.org/data/a.5/weather?q=${cityInputBar}+&appid=${apiKey}&units=imperial`;
+    fetch(requestUrl)
+    .then(function(res) {      
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data)
+      timeZoneOffSet = data.timeZone
+      console.log(timeZoneOffSet)
+      const coordinates= data.coordinates
+      const Latitude = coordinates.Latitude
+      const Longitude = coordinates.Longitude
+    const today = document.querySelector('#currentDay')
+    today.innerHTML=""
+    todayTitle.textContent =data.cityName;
+    todayWeather.textContent=data.weather;
+    todaysTemp.textContent=data.main.temp;
+    todaysHumidity.textContent=data.main.humidity;
+    todaysWind.textContent=data.wind.speed + 'MPH'+ 'windspeed';
+    todaysUV.textContent=data.UV //have to figure out a way to add UV index, Mac said something about second access, using lat & long to get UV
+    //fetches weather icon
+    todaysImg.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}png`);
+
+    today.appendChild()
+    
+    })
+}
+
