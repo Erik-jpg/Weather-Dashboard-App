15b1545f63b58e7151ab491de21f69341a7feac7
Erik Stone <spologas@gmail.com>
2021-09-05 19:37:01 -0700

    found what I thought was a more elegent way to present the five day forcast but now nothing is firing, maybe the code or link for daily is used up? should I have a diffeerent key for each call(x2 per use)?

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const cityInputBar = document.querySelector("#cityInputBar");
 const searchBtn = document.querySelector("#searchBtn");
 const clearHistBtn = document.querySelector("#clearHistBtn");
 const statusMessage = document.querySelector("#statusMessage");
-const searchHistory = document.querySelector("search-history");
-
+const historyEl = document.querySelector("search-history");
+const cityName = document.querySelector("#cityName");
+let searchHistory = JSON.parse(localStorage.getItem('search')) || [];
+console.log(searchHistory);
 let todayTitle = document.createElement("h2");
 let todayUV = document.createElement("h6");
 let toDayTemp = document.createElement("h6");
@@ -14,14 +16,8 @@ const generateMeteorologist = (data) => {
   const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputBar.value}&appid=${APIKey}`;
   fetch(requestUrl)
     .then((response) => {
-      if (response.status === 400 || 404) {
+      if (!response.status === 200) {
         alert("There seems to have been an error, please re-enter a city.");
-        // const error = document.createElement("h1");
-        // errorMessage = "errorMessage";
-        // errorMessage.textContent =
-        //   ;
-        // error.append(errorMessage);console.log("ok");
-        // return;
       }
       return response.json();
     })
@@ -33,7 +29,13 @@ const generateMeteorologist = (data) => {
       console.log(data);
       let today = document.querySelector("#currentDay");
       today.innerHTML = "";
-
+      const currentDate = new Date(data.dt*1000);
+      console.log(currentDate);
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+      cityName.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") "; 
+      
       let todayCard = document.createElement("div");
       let todayBody = document.createElement("div");
       let todayImg = document.createElement("img");
@@ -52,7 +54,7 @@ const generateMeteorologist = (data) => {
 
       todayTitle.textContent = data.name;
       todayWeatherNow.textContent = data.weather[0].description;
-      todayHum.textContent = data.main.humidity;
+      todayHum.textContent = data.main.humidity + "Humidity";
       todayWind.textContent = data.wind.speed + "MPH" + "windspeed";
       
       todayImg.setAttribute(
@@ -81,109 +83,119 @@ return response.json();
     })
     .then(function (data) {
       console.log(data);
-      
-      for (let i = 0; i < data.daily.length; i++) {
-        let fiveDay = document.querySelector("#dayOne");
-        let fiveDayCard = document.createElement("div");
-        let fiveDayBody = document.createElement("div");
-        let fiveDayImg = document.createElement("img");
-        let fiveDayTemp = document.createElement("h6");
-        let fiveDayHum = document.createElement("h6");
-        let fiveDayWind = document.createElement("h6");
-        let fiveDayUV = document.createElement("h6");
-        let fiveDayTitle = document.createElement("h2");
-        let fiveDayWeatherNow = document.createElement("h3");
-        let currentDateAndTime = document.createElement("h4");
+      const fiveDayCard = document.querySelector('.fiveDayForecast');
+      for (let i = 0; i < fiveDayCard.length; i++) {
+        fiveDayCard[i].innerHTML = "";
+        const fiveDayIndex = i*8 + 4;
+        const fiveDayDate = new Date(response.data.list[fiveDayIndex]);
+        const fiveDayDay = fiveDayDate.getDate();
+        const fiveDayMonth = fiveDayDate.getMonth();
+        const fiveDayYear = fiveDayDate.getFullYear();
+        const fiveDayDateEL = document.createElement("p");
+        fiveDayDateEL.setAttribute("class", "fiveDayDate");
+        fiveDayDateEL.innerHTML = fiveDayMonth + '/' + fiveDayDay + '/' + fiveDayYear;
+        fiveDayCard[i].append(fiveDayDateEL);
+        const fiveDayImg = document.createElement('img');
+        fiveDayImg.setAttribute("src", `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`);
+        fiveDayImg.setAttribute("alt", response.data.list[fiveDayIndex].weather[0].description);
+        fiveDayCard[i].append(fiveDayDateEL);
+        const fiveDayTemp = document.createElement("p");
+        fiveDayTemp.innerHTML = 'Temperature: ' + data.daily[i].temp.day;
+        fiveDayCard[i].append(fiveDayTemp);
+        const fiveDayHum = document.createElement("p");
+        fiveDayHum.innerHTML = "Humidity: " + data.daily[i].hum.day;
+        const fiveDayWind = document.createElement("p");
+        fiveDayWind.innerHTML = "wind Speed: " + data.daily[i].wind.speed;
+        // let fiveDay = document.querySelector("#dayOne");
+        // let fiveDayCard = document.createElement("div");
+        // let fiveDayBody = document.createElement("div");
+        // let fiveDayImg = document.createElement("img");
+        // let fiveDayTemp = document.createElement("h6");
+        // let fiveDayHum = document.createElement("h6");
+        // let fiveDayWind = document.createElement("h6");
+        // let fiveDayUV = document.createElement("h6");
+        // let fiveDayTitle = document.createElement("h2");
+        // let fiveDayWeatherNow = document.createElement("h3");
+        // let currentDateAndTime = document.createElement("h4");
 
-        fiveDayCard.classList = "card";
-        fiveDayBody.classList = "card-body";
-        fiveDayTemp.classList = "card-text";
-        fiveDayWind.classList = "card-text";
-        fiveDayHum.classList = "card-text";
-        fiveDayUV.classList = "card-text";
-        fiveDayTitle.classList = "card-text";
-        fiveDayWeatherNow.classList = "card-text";
-        currentDateAndTime.classList = "card-text";
+        // fiveDayCard.classList = "card";
+        // fiveDayBody.classList = "card-body";
+        // fiveDayTemp.classList = "card-text";
+        // fiveDayWind.classList = "card-text";
+        // fiveDayHum.classList = "card-text";
+        // fiveDayUV.classList = "card-text";
+        // fiveDayTitle.classList = "card-text";
+        // fiveDayWeatherNow.classList = "card-text";
+        // currentDateAndTime.classList = "card-text";
 
         // fiveDayTitle.textContent = data.city.cityName;
-        fiveDayTemp.textContent = data.daily[i].temp.day;
-        fiveDayHum.textContent = data.daily[i].humidity;
-        fiveDayWind.textContent = data.daily[i].wind_speed;
-        todayUV.textContent = data.current.uvi;
-        toDayTemp.textContent = data.daily[0].temp.day;
+        // fiveDayTemp.textContent = data.daily[i].temp.day + "Temperature";
+        // fiveDayHum.textContent = data.daily[i].humidity + "Humidity";
+        // fiveDayWind.textContent = data.daily[i].wind_speed + "Wind Speed";
+        todayUV.textContent = data.current.uvi + "UVIndex";
+        toDayTemp.textContent = data.daily[0].temp.day + "Today's Current Temperature";
 
-        fiveDayImg.setAttribute(
-          "src",
-          `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`
-        );console.log("fiveDayImg");
+        // fiveDayImg.setAttribute(
+        //   "src",
+          
+        // );console.log("fiveDayImg");
         
 
         // todayBody.appendChild(today);
-        fiveDayCard.append(fiveDayBody);
-        fiveDay.append(fiveDayTitle);
-        fiveDay.append(currentDateAndTime);
-        fiveDayTitle.append(fiveDayImg);
-        fiveDay.append(fiveDayTemp);
-        fiveDay.append(fiveDayHum);
-        fiveDay.append(fiveDayWind);
-        fiveDay.append(fiveDayUV);
+        // fiveDayCard.append(fiveDayBody);
+        // fiveDay.append(fiveDayTitle);
+        // fiveDay.append(currentDateAndTime);
+        // fiveDayTitle.append(fiveDayImg);
+        // fiveDay.append(fiveDayTemp);
+        // fiveDay.append(fiveDayHum);
+        // fiveDay.append(fiveDayWind);
+        // fiveDay.append(fiveDayUV);
       }
     });
 }
 
 function addToStorage() {
-if(localStorage.getItem('cachedHistory') || []) {
-  const historyArr = [cityInputBar.value];
-  localStorage.setItem('cachedHistory', JSON.stringify(historyArr));
-  console.log(localStorage.getItem('cachedHistory'));
-  const h2History = document.createElement('li');
-  h2History.classList.add('button');
-  h2History.textContent = historyArr;
-  h2History.append('historyKey');
-  // historyKey.push(historyArr);
-  h2History.addEventListener('click', (event) => {
-  // cityInputBar = event.target.textContent;
-  //should I change the name of this request, so it doesn't fire when original search is clicked?
-  const weatherHistory = `https://api.openweathermap.org/data/2.5/weather?q=${h2History.value}&appid=${APIKey}`;
-  generateMeteorologist(weatherHistory);
-});
+  historyEl.innerText="";
+  for (var i = 0; i < searchHistory.length; i++) {
+    const historyKey = document.querySelector("#cityInputBar");
+    historyKey.setAttribute("value", searchHistory[i]);
+    historyKey.addEventListener("click", function(){
+      generateMeteorologist(historyKey.value);
+      console.log(localStorage.getItem('historyKey'));
+    })
+    historyEl.append(historyKey);
+  }
 }
 
-}
 function checkHistory(){
- historyArr = JSON.parse(localStorage.getItem('cachedHistory'));
- console.log(localStorage);
- while (historyArr[historyArr.length - 1] !== todayTitle && historyArr.length) 
- if (!historyArr.length) {
-   const newHistoryArr = JSON.parse(localStorage.getItem('cachedHistory'));
-   localStorage.setItem('cachedHistory', JSON.stringify(newHistoryArr));
-   newHistoryArr.push(historyArr)
- } else {
-  return; 
+if (searchHistory.length > 0){
+  generateMeteorologist(searchedCity[searchHistory.length - 1]);
 }
 }
-
-// addToStorage();
-
-// const addToHistory = () => {
-//   if (localStorage.getItem('cachedHistory')) {
-
-//   }
-// }
 
 function deleteHistory() {
-  localStorage.clear(todayTitle);
+  localStorage.clear(searchedCity);
   
 } 
 
-searchBtn.addEventListener("click", everythingToDo);
+searchBtn.addEventListener("click", function() {
+  const searchedCity = cityInputBar.value;
+  generateMeteorologist(searchedCity);
+  searchHistory.push(searchedCity);
+  localStorage.setItem("search", JSON.stringify(searchHistory));
+  fiveDayForcast();
+});
 
-clearHistBtn.addEventListener('click', deleteHistory);
+clearHistBtn.addEventListener('click', function(){
+  localStorage.clear();
+  searchHistory = [];
 
-function everythingToDo() {
-  generateMeteorologist();
+});
+
+// function everythingToDo() {
   
-  // console.log(localStorage.getItem('cachedHistory'));
-  addToStorage
-  checkHistory
-}
+  
+//   // 
+//   addToStorage
+//   checkHistory
+// }
